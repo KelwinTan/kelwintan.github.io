0cbe7a5d97ea4bde4b70596873554160dffc2ba4
kelwintan <lw.kelwin@gmail.com>
2024-03-07 18:05:21 +0700

    test: remove config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  reactStrictMode: true,
-  // Add basePath
-  basePath: '/github-pages',
-  distDir: 'out',
 }
 
 // const withMDX = nextMDX({
